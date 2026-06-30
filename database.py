e9d209e923cabba39976297ebf5375a724436ea3
Seenu <ch24btech11035@iith.ac.in>
2026-06-30 19:50:09 +0530

    Removed SQLite specific thread args for Postgres

--- database.py
+++ database.py
@@ -10,10 +10,7 @@ SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 if not SQLALCHEMY_DATABASE_URL:
     raise ValueError("CRITICAL ERROR: DATABASE_URL is missing!")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args = {"check_same_thread" : False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
